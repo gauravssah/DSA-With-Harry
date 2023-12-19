d90569873ce07d5dd2059d31f67d55b501ede40c
gauravssah <124705806+gauravssah@users.noreply.github.com>
2023-12-20 00:25:45 +0530

    Linked List Started

---
+++ V-14/01Creation_of_Linked_List.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+void linkedListTraversal(struct Node *ptr)
+{
+    while (ptr != NULL)
+    {
+        cout << "The Value is : " << ptr->data << endl;
+        ptr = ptr->next;
+    }
+}
+
+int main()
+{
+    struct Node *head;
+    struct Node *second;
+    struct Node *third;
+    struct Node *fourth;
+
+    // Allocate memory for nodes in the linked list in Heap
+    head = (struct Node *)malloc(sizeof(struct Node));
+    second = (struct Node *)malloc(sizeof(struct Node));
+    third = (struct Node *)malloc(sizeof(struct Node));
+    fourth = (struct Node *)malloc(sizeof(struct Node));
+
+    // Link first and second nodes
+    head->data = 7;
+    head->next = second;
+
+    // Link second and third nodes
+    second->data = 12;
+    second->next = third;
+
+    // Link third and fourth nodes
+    third->data = 22;
+    third->next = fourth;
+
+    // Terminate the list at the fourth node
+    fourth->data = 40;
+    fourth->next = NULL;
+
+    linkedListTraversal(head);
+
+    return 0;
+}
