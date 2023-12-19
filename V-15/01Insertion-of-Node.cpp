d39783b22dd920a4789bbd07846ba6828ccf2251
gauravssah <124705806+gauravssah@users.noreply.github.com>
2023-12-20 00:47:15 +0530

    insertion

---
+++ V-15/01Insertion-of-Node.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int main()
+{
+
+    return 0;
+};
+
+// Insertion in a Linked List.
+
+// Case 1: Insert at the beginning.  ---> O(1)
+// Case 2: Insert at the between.    ---> O(n)
+// Case 3: Insert at the end.        ---> O(n)
+// Case 4: Insert After a node.      ---> O(1)
